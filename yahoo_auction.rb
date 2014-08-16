c8c2ea0a1f2561e54cc06a50333bb8bb4f911ca0
AKB428 <otoraru@gmail.com>
2014-08-17 06:38:04 +0900

    add search json

--- yahoo_auction.rb
+++ yahoo_auction.rb
@@ -18,9 +18,9 @@ if @twitter_flag
 end
 
 File.open './conf/conf.json' do |file|
-   @conf = JSON.load(file.read)
-   @yahoo_conf = @conf["YahooJapan"]
-   @mongo_conf = @conf["MongoDB"]
+  @conf = JSON.load(file.read)
+  @yahoo_conf = @conf["YahooJapan"]
+  @mongo_conf = @conf["MongoDB"]
 end
 
 if @mongo_conf["exec"]
@@ -39,43 +39,38 @@ max = 10
 #1回のツイートで間隔をあける秒数
 tweet_sleep_time = 10
 
-search_target = {word: "一眼", hash_tag: "#一眼レフ"}
-search_param1 = {appid: application_key, query: "一眼" , sort: "bids", order: "a", page: "1"} 
-search_param2 = {appid: application_key, query: "一眼" , order: "a", page: "1"} 
-
 def get_data(conf, search_target, param)
-  p param
   res = Net::HTTP.post_form(URI.parse(@yahoo_url), param)
   result = jsonp_decode res.body
-  
+
   item_list = result["ResultSet"]["Result"]["Item"]
   #p item_list
-    
-  tweet_list = []  
-  item_list.each do |item|
-     title = item["Title"]
-     #入札数
-     bids = item["Bids"] || 0
-     bids = "入札数=" + bids  
-       
-     encded_auc_url = URI.escape item["AuctionItemUrl"];
-     affi_url = @affiliate_url + encded_auc_url
 
-     sokketu = "即決価格=なし"
-     sokketu = sprintf( "即決価格=%d円", item["BidOrBuy"].to_i ) if item["BidOrBuy"] != nil
-     #sokketu = item["BidOrBuy"].to_s
-     
-     #rfc3339形式なので変換する
-     end_time = Time.parse item["EndTime"]
-     format_end_time = "終了時間=" + end_time.strftime("%Y年%m月%d日 %H:%M:%S")
-     
-     current_price = sprintf( "現在価格=%d円", item["CurrentPrice"].to_i )
-    
-     result = title + " " + bids +  " " + current_price + " " + affi_url + " " + sokketu + " " + format_end_time + " " + search_target[:hash_tag]
-     tweet_list.push result
-     
-     #mongoDBに挿入
-     @mongo_collection.insert(item) if @mongo_conf["exec"]
+  tweet_list = []
+  item_list.each do |item|
+    title = item["Title"]
+    #入札数
+    bids = item["Bids"] || 0
+    bids = "入札数=" + bids
+
+    encded_auc_url = URI.escape item["AuctionItemUrl"];
+    affi_url = @affiliate_url + encded_auc_url
+
+    sokketu = "即決価格=なし"
+    sokketu = sprintf( "即決価格=%d円", item["BidOrBuy"].to_i ) if item["BidOrBuy"] != nil
+    #sokketu = item["BidOrBuy"].to_s
+
+    #rfc3339形式なので変換する
+    end_time = Time.parse item["EndTime"]
+    format_end_time = "終了時間=" + end_time.strftime("%Y年%m月%d日 %H:%M:%S")
+
+    current_price = sprintf( "現在価格=%d円", item["CurrentPrice"].to_i )
+
+    result = title + " " + bids +  " " + current_price + " " + affi_url + " " + sokketu + " " + format_end_time + " " + search_target["hash_tag"]
+    tweet_list.push result
+
+    #mongoDBに挿入
+    @mongo_collection.insert(item) if @mongo_conf["exec"]
   end
 
   tweet_list.each do |tweet_string|
@@ -92,8 +87,8 @@ def get_data(conf, search_target, param)
       puts e.to_s
     ensure
       if @twitter_flag
-        puts "sleep 10"
-        sleep 10
+        puts "sleep " + tweet_sleep_time.to_s
+        sleep tweet_sleep_time
       end
     end
   end
@@ -105,12 +100,13 @@ def jsonp_decode(jsonp)
   result = JSON.load(result_jsonp)
 end
 
-get_data(@yahoo_conf, search_target, search_param1)
+File.open './conf/search_param.json' do |file|
+  search_param = JSON.load(file.read)
+  @search_target = search_param["search_target"]
+  @search_params = search_param["search_params"]
+end
 
-
-
-
-
-
-#puts result_jsonp
-#get_data(@yahoo_conf, search_target, search_param2)
+@search_params.each do |search_param|
+  search_param['appid'] = application_key
+  get_data(@yahoo_conf, @search_target, search_param)
+end
