0d859ce83b1beda44d8c39dc7e84f3b7e72047d5
AKB428 <otoraru@gmail.com>
2014-08-25 22:31:29 +0900

    fix loop

--- loop.rb
+++ loop.rb
@@ -1,5 +1,5 @@
 sleep_minute = ARGV.shift.to_i
 while (true) do
-        puts "bundle exec ruby yahoo_auction.rb " + ARGV.join(" ")
+        system "bundle exec ruby yahoo_auction.rb " + ARGV.join(" ")
         sleep(sleep_minute * 60)#60*60
 end
